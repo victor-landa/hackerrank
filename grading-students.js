61634da135661099d653162710b5e8120c473ad0
victor-landa <victorlanda.dev@gmail.com>
2021-06-23 21:48:12 -0500

    grading students

---
+++ grading-students.js
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/grading/problem
+
+const grades = [73, 67, 38, 33];
+
+function gradingStudents(grades) {
+  const finalGrades = [];
+
+  grades.map(grade => {
+    if(grade >= 38) {
+      const nextMultiple = Math.ceil(grade/5.0) * 5;
+      const logicRound = nextMultiple - grade;
+      if(logicRound >= 3) {
+        finalGrades.push(grade);
+      } else {
+        finalGrades.push(nextMultiple);
+      }
+    } else {
+      finalGrades.push(grade);
+    }
+  });
+
+  return finalGrades;
+}
+
+gradingStudents(grades);
